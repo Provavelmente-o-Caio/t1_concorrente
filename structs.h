7ae1c655f250d5648c5dfbcac7014b3df95f2df0
lucas <lucbsm@gmail.com /usr/bin/env /bin/sh /tmp/Microsoft-MIEngine-Cmd-ci1s3hgy.o1j>
2023-10-30 20:11:07 -0300

    struct

---
+++ structs.h
@@ -0,0 +1,26 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <semaphore.h>
+#include "fila.h"
+
+typedef struct {
+    int id;
+    sem_t sem;
+} cliente_t;
+
+typedef struct {
+    int id;
+    int num_pedido;
+    int terminou_rodada;
+    sem_t sem;
+    int* fila_clientes;
+} garcom_t;
+
+typedef struct {
+    int rodadas;
+    Queue fila_clientes;
+} bar_t;
+
+
+#endif
